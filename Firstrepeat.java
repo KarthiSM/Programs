5dd1fb8037f5e3bda91e33929979f23eedd7c332
KarthiSM <karthism312@gmail.com>
2017-06-14 10:23:29 +0530

    Create Firstrepeat.java

---
+++ Firstrepeat.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Firstrepeat
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n,i,j;
+        n=s.nextInt();
+        int[] a=new int[n];
+        int[] b=new int[1];
+        for(i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        for(i=0;i<n-1;i++)
+        {
+            if(a[i]==a[i+1])
+                {
+                b[0]=a[i];    
+                }
+        }
+        System.out.println(b[0]);
+    }
+}
+
+   
+
